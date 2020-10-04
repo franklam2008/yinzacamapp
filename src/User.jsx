e138523790085b81bfd34263fbd30556e2379176
franklam2008 <franklam2008@gmail.com>
2020-10-04 16:51:49 -0400

    remove files

--- src/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-export default function User({ user }) {
-  return (
-    <div className="userCon">
-      <img src={user.avatar_url} alt="" className="userAvatarIcon" />
-      <p className="userLogin" >{user.login}</p>
-    </div>
-  );
-}
